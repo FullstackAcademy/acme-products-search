ad8fd09e78fbc5a6b90ab7a74465d8c6682f22ca
ericpkatz <ericpkatz@gmail.com>
2022-09-27 18:46:40 -0400

    first commit

---
+++ db/index.js
@@ -0,0 +1,54 @@
+const Sequelize = require('sequelize');
+const { UUID, UUIDV4, STRING, DECIMAL, INTEGER } = Sequelize;
+
+const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_products_db');
+
+const Product = conn.define('product', {
+  id: {
+    type: UUID,
+    defaultValue: UUIDV4,
+    primaryKey: true
+  },
+  name: {
+    type: STRING(20),
+    allowNull: false,
+    unique: true,
+    validate: {
+      notEmpty: true,
+      len: [1, 21]
+    }
+  },
+  price: {
+    type: DECIMAL,
+    allowNull: false,
+    validate: {
+      isDecimal: true,
+      isPositive: (value)=> {
+        if(value <= 0){
+          throw 'price must be positive'
+        }
+      }
+    }
+  },
+  numberInStock: {
+    type: INTEGER,
+    allowNull: false,
+    validate: {
+      min: 0,
+      isInt: true
+    }
+  }
+});
+
+const seed = ()=> {
+  return Promise.all([
+    Product.create({ name: 'foo', price: 2.99, numberInStock: 7}),
+    Product.create({ name: 'bar', price: 2.99, numberInStock: 7}),
+  ]);
+};
+
+module.exports = {
+  conn,
+  seed,
+  Product
+};
